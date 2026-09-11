3b87f98a0ae21d0b34350a6c598ce9d68a0d3080
Imran Gul <gulimran735@gmail.com>
2026-09-12 02:22:51 +0500

    feat: add alphabet page to sitemap

--- armenian-keyboard/src/app/sitemap.ts
+++ armenian-keyboard/src/app/sitemap.ts
@@ -1,4 +1,18 @@
 import type { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: "https://armeniankeyboard.com/", lastModified: new Date(), changeFrequency: "monthly", priority: 1 }];
+  return [
+    {
+      url: "https://armeniankeyboard.com/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://armeniankeyboard.com/armenian-alphabet",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
 }
